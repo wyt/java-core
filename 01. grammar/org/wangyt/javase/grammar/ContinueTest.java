d50d8251e4f79f5da56d383e1f3ce7c9762e66bd
wangyongtao <wangyt4dev@gmail.com>
2018-03-05 16:48:00 +0800

    optimized code.

--- 01. grammar/org/wangyt/javase/grammar/ContinueTest.java
+++ 01. grammar/org/wangyt/javase/grammar/ContinueTest.java
@@ -3,22 +3,21 @@ package org.wangyt.javase.grammar;
 /**
  * @author 王永涛
  * 
- * @date 2012-7-13 下午01:55:24
+ * @since 2012-7-13 下午01:55:24
  * 
- * @version v1.0
- * 
- * @Copyright (c) 2012,WANGYT. All Rights Reserved.
+ *        Copyright (c) 2012,WANGYT. All Rights Reserved.
  */
 public class ContinueTest {
-	public static void main(String[] args) {
-		int m = 3;
-		for (int i = 0; i < 8; i++) {
-			System.out.print(i);
-			if (i == m) {
-				// 条件为真的时候，会跳过其后的语句，进入下一次循环。
-				continue;
-			}
-			System.out.println("$");
-		}
-	}
+  public static void main(String[] args) {
+    int m = 3;
+    for (int i = 0; i < 8; i++) {
+      System.out.print(i);
+      if (i == m) {
+        System.out.println("");
+        // 条件为真的时候，会跳过其后的语句，进入下一次循环。
+        continue;
+      }
+      System.out.println("$");
+    }
+  }
 }
