1f1275170b6d1be9f1ea6c7030ab43101c4b19cb
wangyongtao <wangyt4dev@gmail.com>
2018-04-09 13:47:24 +0800

    optimized code.

--- itcast/thread/cn/itcast/heima2/L03_TraditionalThreadSynchronized.java
+++ itcast/thread/cn/itcast/heima2/L03_TraditionalThreadSynchronized.java
@@ -6,9 +6,8 @@ package cn.itcast.heima2;
  * 
  * 1. 非静态同步方法用的都是同一把锁——实例对象本身. 
  * 2. 静态同步方法用的也是同一把锁——类对象本身 
- * 3. 同步代码块可以用实例对象本身(this);也可以用类对象本身T.class 4.
- * 只有用同一把锁的方法或者代码块之间,才会相互构成锁竞争条件.
- * 
+ * 3. 同步代码块可以用实例对象本身(this);也可以用类对象本身T.class 
+ * 4. 只有用同一把锁的方法或者代码块之间,才会相互构成锁竞争条件.
  * </P>
  * 
  * @author WANG YONG TAO
@@ -25,45 +24,34 @@ public class L03_TraditionalThreadSynchronized {
 
     final Outputer outputer = new Outputer();
 
-    new Thread(new Runnable() {
-      @Override
-      public void run() {
-        while (true) {
-          try {
-            Thread.sleep(2000);
-          } catch (InterruptedException e) {
-            e.printStackTrace();
-          }
-          outputer.output2("zhangxiaoxiang");
+    new Thread(() -> {
+      while (true) {
+        try {
+          Thread.sleep(2000);
+        } catch (InterruptedException e) {
+          e.printStackTrace();
         }
+        outputer.output2("How are you?");
+      }
+    }).start();;
+
+    new Thread(() -> {
+      while (true) {
+        try {
+          Thread.sleep(2000);
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+        outputer.output("Fine,thank you.");
       }
     }).start();
-
-    new Thread(new Runnable() {
-      @Override
-      public void run() {
-        while (true) {
-          try {
-            Thread.sleep(2000);
-          } catch (InterruptedException e) {
-            e.printStackTrace();
-          }
-          outputer.output("lihuoming");
-        }
-
-      }
-    }).start();
-
   }
 
   static class Outputer {
 
     public void output(String name) {
       int len = name.length();
-
-      /** 相当于静态的同步方法 */
-      // synchronized (Outputer.class) {
-      /** 相当于非静态的同步方法 */
+      // 当前实例对象
       synchronized (this) {
         for (int i = 0; i < len; i++) {
           System.out.print(name.charAt(i));
@@ -72,6 +60,7 @@ public class L03_TraditionalThreadSynchronized {
       }
     }
 
+    // 和output方法同一个锁监视器
     public synchronized void output2(String name) {
       int len = name.length();
       for (int i = 0; i < len; i++) {
@@ -80,6 +69,7 @@ public class L03_TraditionalThreadSynchronized {
       System.out.println();
     }
 
+    // output3使用Outputer.class,作为锁监视器.
     public static synchronized void output3(String name) {
       int len = name.length();
       for (int i = 0; i < len; i++) {
