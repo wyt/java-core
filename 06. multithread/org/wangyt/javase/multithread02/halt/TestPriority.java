fe00d9a35b188a88dd303276d07fe00d242c0d40
wangyongtao <wangyt4dev@gmail.com>
2018-04-09 11:17:43 +0800

    optimized code.

--- 06. multithread/org/wangyt/javase/multithread02/halt/TestPriority.java
+++ 06. multithread/org/wangyt/javase/multithread02/halt/TestPriority.java
@@ -9,39 +9,39 @@ package org.wangyt.javase.multithread02.halt;
  * 
  */
 class TestPriority extends Thread {
-	public TestPriority() {
-	}
 
-	public TestPriority(String name) {
-		super(name);
-	}
+  public TestPriority() {}
 
-	public void run() {
-		for (int i = 0; i < 50; i++) {
-			System.out.println(getName() + ",其优先级是: " + this.getPriority() + ",循环变量的值为： " + i);
-		}
-	}
+  public TestPriority(String name) {
+    super(name);
+  }
 
-	public static void main(String[] args) {
-		// 改变main的优先级
-		Thread.currentThread().setPriority(6);
+  public void run() {
+    for (int i = 0; i < 50; i++) {
+      System.out.println(getName() + ",其优先级是: " + this.getPriority() + ",循环变量的值为： " + i);
+    }
+  }
 
-		for (int i = 0; i < 30; i++) {
-			if (i == 10) {
-				// low线程的优先级默认和创建它的父线程main-thread相同。
-				Thread low_thread = new TestPriority("low-thread");
+  public static void main(String[] args) {
+    // 改变main的优先级
+    Thread.currentThread().setPriority(6);
 
-				low_thread.start();
-				System.out.println("low-thread,创建之初的优先级: " + low_thread.getPriority());
-				low_thread.setPriority(Thread.MIN_PRIORITY);
-			}
-			if (i == 20) {
-				// high线程的优先级默认和创建它的父线程main-thread相同。
-				Thread high_thread = new TestPriority("high-thread");
-				high_thread.start();
-				System.out.println("high-thread,创建之初的优先级: " + high_thread.getPriority());
-				high_thread.setPriority(Thread.MAX_PRIORITY);
-			}
-		}
-	}
+    for (int i = 0; i < 30; i++) {
+      if (i == 10) {
+        // low线程的优先级默认和创建它的父线程main-thread相同。
+        Thread lowThread = new TestPriority("low-thread");
+        lowThread.start();
+        System.out.println("low-thread,创建之初的优先级: " + lowThread.getPriority());
+        lowThread.setPriority(Thread.MIN_PRIORITY);
+      }
+
+      if (i == 20) {
+        // high线程的优先级默认和创建它的父线程main-thread相同。
+        Thread highThread = new TestPriority("high-thread");
+        highThread.start();
+        System.out.println("high-thread,创建之初的优先级: " + highThread.getPriority());
+        highThread.setPriority(Thread.MAX_PRIORITY);
+      }
+    }
+  }
 }
