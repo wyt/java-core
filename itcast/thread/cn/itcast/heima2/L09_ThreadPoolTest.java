73ab9b1fa907ea80bb3374d3d0aafe498a4f8e32
wangyongtao <wangyt4dev@gmail.com>
2018-04-09 14:11:35 +0800

    optimized code.

--- itcast/thread/cn/itcast/heima2/L09_ThreadPoolTest.java
+++ itcast/thread/cn/itcast/heima2/L09_ThreadPoolTest.java
@@ -5,7 +5,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 /**
- * Executors
+ * ExecutorService,线程池.
  * 
  * @author WANG YONG TAO
  *
@@ -15,24 +15,21 @@ public class L09_ThreadPoolTest {
   public static void main(String[] args) {
 
     /** 三种不同的线程池 **/
-    ExecutorService threadPool = Executors.newFixedThreadPool(3);
     // ExecutorService threadPool = Executors.newCachedThreadPool();
     // ExecutorService threadPool = Executors.newSingleThreadExecutor();
+    ExecutorService threadPool = Executors.newFixedThreadPool(3);
 
     for (int i = 1; i <= 10; i++) {
       final int task = i;
-      threadPool.execute(new Runnable() {
-        @Override
-        public void run() {
-          for (int j = 1; j <= 10; j++) {
-            try {
-              Thread.sleep(20);
-            } catch (InterruptedException e) {
-              e.printStackTrace();
-            }
-            System.out.println(
-                Thread.currentThread().getName() + " is looping of " + j + " for  task of " + task);
+      threadPool.execute(() -> {
+        for (int j = 1; j <= 10; j++) {
+          try {
+            Thread.sleep(20);
+          } catch (InterruptedException e) {
+            e.printStackTrace();
           }
+          System.out.println(
+              Thread.currentThread().getName() + " is looping of " + j + " for  task of " + task);
         }
       });
     }
@@ -55,4 +52,5 @@ public class L09_ThreadPoolTest {
       }
     }, 6, 2, TimeUnit.SECONDS);
   }
+
 }
