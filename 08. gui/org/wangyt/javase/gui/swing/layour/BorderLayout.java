fd7f2b912bdde463a23fdfbfcf2833d3aa01368b
wangyongtao <wangyt4dev@gmail.com>
2018-04-10 15:35:51 +0800

    optimized code.

--- 08. gui/org/wangyt/javase/gui/swing/layour/BorderLayout.java
+++ 08. gui/org/wangyt/javase/gui/swing/layour/BorderLayout.java
@@ -3,7 +3,6 @@ package org.wangyt.javase.gui.swing.layour;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Toolkit;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JTextArea;
@@ -11,44 +10,45 @@ import javax.swing.JTextArea;
 /**
  * 东西南北中布局，布局都是可以嵌套的。
  * 
- * @author 王永涛
+ * @author WANG YONG TAO
  * 
  * @since 2012-9-2 下午02:00:08
  * 
  */
 public class BorderLayout {
-	
-	public static void main(String[] args) {
-		JFrame jf = new JFrame("BorderLayout");
-		jf.setLayout(new java.awt.BorderLayout());
-		jf.add(new JButton("东"), java.awt.BorderLayout.EAST);
-		// jf.add(new JButton("中"), java.awt.BorderLayout.CENTER);
-		jf.add(new JTextArea("中"), java.awt.BorderLayout.CENTER);
-		jf.add(new JButton("南"), java.awt.BorderLayout.SOUTH);
-		jf.add(new JButton("北"), java.awt.BorderLayout.NORTH);
-		jf.add(new JButton("西"), java.awt.BorderLayout.WEST);
 
-		showWindow(jf);
-	}
+  public static void main(String[] args) {
+    JFrame jf = new JFrame("BorderLayout");
+    jf.setLayout(new java.awt.BorderLayout());
+    jf.add(new JButton("东"), java.awt.BorderLayout.EAST);
+    // jf.add(new JButton("中"), java.awt.BorderLayout.CENTER);
+    jf.add(new JTextArea("中"), java.awt.BorderLayout.CENTER);
+    jf.add(new JButton("南"), java.awt.BorderLayout.SOUTH);
+    jf.add(new JButton("北"), java.awt.BorderLayout.NORTH);
+    jf.add(new JButton("西"), java.awt.BorderLayout.WEST);
 
-	/**
-	 * 显示窗体的方法
-	 * 
-	 * @param jf
-	 */
-	public static void showWindow(JFrame jf) {
-		jf.setSize(500, 300);
-		jf.getContentPane().setBackground(new Color(236, 233, 216));
+    showWindow(jf);
+  }
 
-		// 屏幕大小
-		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-		Dimension frameSize = jf.getSize();
-		// 设置窗口在屏幕中的位置
-		jf.setLocation((screenSize.width - frameSize.width) / 2, (screenSize.height - frameSize.height) / 2);
-		// 窗口尺寸是否可变
-		jf.setResizable(false);
-		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		jf.setVisible(true);
-	}
-	
+  /**
+   * 显示窗体的方法
+   * 
+   * @param jf
+   */
+  public static void showWindow(JFrame jf) {
+    jf.setSize(800, 500);
+    jf.getContentPane().setBackground(new Color(236, 233, 216));
+
+    // 屏幕大小
+    Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    Dimension frameSize = jf.getSize();
+    // 设置窗口在屏幕中的位置
+    jf.setLocation((screenSize.width - frameSize.width) / 2,
+        (screenSize.height - frameSize.height) / 2);
+    // 窗口尺寸是否可变
+    jf.setResizable(false);
+    jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    jf.setVisible(true);
+  }
+
 }
--- 08. gui/org/wangyt/javase/gui/swing/layour/FlowLayout.java
+++ 08. gui/org/wangyt/javase/gui/swing/layour/FlowLayout.java
@@ -3,48 +3,48 @@ package org.wangyt.javase.gui.swing.layour;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Toolkit;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
 /**
- * FlowLayout 從左向右順序排列
+ * FlowLayout 流布局，从左向右顺序排列
  * 
- * @author 王永涛
+ * @author WANG YONG TAO
  * 
  * @since 2012-9-2 下午02:08:54
  * 
  */
 public class FlowLayout {
-	
-	public static void main(String[] args) {
-		JFrame jf = new JFrame("FlowLayout");
-		jf.setLayout(new java.awt.FlowLayout());
-		for (int i = 0; i < 10; i++) {
-			jf.add(new JButton(i + ""));
-		}
 
-		showWindow(jf);
-	}
+  public static void main(String[] args) {
+    JFrame jf = new JFrame("FlowLayout");
+    jf.setLayout(new java.awt.FlowLayout());
+    for (int i = 0; i < 10; i++) {
+      jf.add(new JButton(i + ""));
+    }
 
-	/**
-	 * 显示窗体的方法
-	 * 
-	 * @param jf
-	 */
-	public static void showWindow(JFrame jf) {
-		jf.setSize(500, 300);
-		jf.getContentPane().setBackground(new Color(236, 233, 216));
+    showWindow(jf);
+  }
 
-		// 屏幕大小
-		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-		Dimension frameSize = jf.getSize();
-		// 设置窗口在屏幕中的位置
-		jf.setLocation((screenSize.width - frameSize.width) / 2, (screenSize.height - frameSize.height) / 2);
-		// 窗口尺寸是否可变
-		jf.setResizable(false);
-		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		jf.setVisible(true);
-	}
-	
+  /**
+   * 显示窗体的方法
+   * 
+   * @param jf
+   */
+  public static void showWindow(JFrame jf) {
+    jf.setSize(800, 500);
+    jf.getContentPane().setBackground(new Color(236, 233, 216));
+
+    // 屏幕大小
+    Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    Dimension frameSize = jf.getSize();
+    // 设置窗口在屏幕中的位置
+    jf.setLocation((screenSize.width - frameSize.width) / 2,
+        (screenSize.height - frameSize.height) / 2);
+    // 窗口尺寸是否可变
+    jf.setResizable(false);
+    jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    jf.setVisible(true);
+  }
+
 }
